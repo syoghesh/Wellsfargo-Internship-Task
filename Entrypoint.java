672ea2e3e60f27be5387e881cddfa506025780a8
syoghesh <124021113+syoghesh@users.noreply.github.com>
2024-04-22 23:24:19 +0530

    Entrypoint.java

---
+++ Entrypoint.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.counselor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entrypoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Entrypoint.class, args);
+    }
+
+}
